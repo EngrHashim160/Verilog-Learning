c0fc3a619847eb5138f663803ad46e5825067c13
EngrHashim160 <engrhashim160@gmail.com>
2026-05-22 00:48:14 +0500

    Practice AND Gate

---
+++ gates/and_gate/testbench.v
@@ -0,0 +1,25 @@
+module testbench;
+
+    reg a;
+    reg b;
+    wire y;
+
+    and_gate uut (
+        .a(a),
+        .b(b),
+        .y(y)
+    );
+
+    initial begin
+        $dumpfile("dump.vcd");
+        $dumpvars(0, testbench);
+
+        a = 0; b = 0; #10;
+        a = 0; b = 1; #10;
+        a = 1; b = 0; #10;
+        a = 1; b = 1; #10;
+
+        $finish;
+    end
+
+endmodule
