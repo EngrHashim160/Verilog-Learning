4fb847a6eb67f59a02513bf187b0053dd17b85d4
EngrHashim160 <engrhashim160@gmail.com>
2026-05-22 00:49:26 +0500

    Practice OR gate

---
+++ gates/or_gate/testbench.v
@@ -0,0 +1,26 @@
+module testbench;
+
+    reg  A;
+    reg  B;
+
+    wire Y;
+
+    or_gate uut(
+        .A(A),
+        .B(B),
+        .Y(Y)
+    );
+
+    initial begin
+        $dumpfile("dump.vcd");
+        $dumpvars(0, testbench);
+
+        A = 0; B = 0; #10;
+        A = 0; B = 1; #10;
+        A = 1; B = 0; #10;
+        A = 1; B = 1; #10;
+
+        $finish;
+    end
+
+endmodule
