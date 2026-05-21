a9ed14d3989818e8dfd05857a3f4871484e6caf8
EngrHashim160 <engrhashim160@gmail.com>
2026-05-22 00:51:45 +0500

    NAND Gate Practiced & Implemented

---
+++ gates/nand_gate/testbench.v
@@ -0,0 +1,26 @@
+module testbench;
+    reg a;
+    reg b;
+    wire y;
+
+    nand_gate uut (
+        .a(a),
+        .b(b),
+        .y(y)
+    );
+
+    initial begin
+        $dumpfile("dump.vcd");
+        $dumpvars(0, testbench);
+
+        a=0; b=0; #10;
+        a=0; b=1; #10;
+        a=1; b=0; #10;
+        a=1; b=1; #10;
+
+        $finish;
+    end
+
+
+endmodule
+
